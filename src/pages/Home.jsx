410da3065cf3db904240074dd9aa19a5d8c85c44
deevfer <vasquez.fernando94@gmail.com>
2026-02-24 21:38:55 -0600

    Home con botones spotify y apple music

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,7 +8,40 @@ function Home() {
 
   return (
     <div className="landing">
-        <h1>Logeado</h1>
+        <div className="container">
+            <div className="loginForm">
+                <h1>Conecta tu música</h1>
+                <span>Permite que Sona acceda a tu biblioteca para controlar la pista actual en la aplicación.</span>
+                <div className="btnsConect">
+                    <div className="btnPrimary">
+                        <button>
+                            <img src="/spotify.png" alt="" />
+                            Spotify
+                        </button>
+                    </div>
+                    <div className="btnPrimary">
+                        <button>
+                            <img src="/AppleMusic.png" alt="" />
+                            Apple Music
+                        </button>
+                        <div className="comingSoon">
+                            <span>Coming soon</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div className="vinylBottom">
+        <div className="leftVinyl">
+            <img src="/leftVinyl.svg" alt="" />
+        </div>
+        <div className="centerVinyl">
+            <img src="/centerVinyl.svg" alt="" />
+        </div>
+        <div className="rightVinyl">
+            <img src="/rightVinyl.svg" alt="" />
+        </div>
+        </div>
     </div>
   )
 }
